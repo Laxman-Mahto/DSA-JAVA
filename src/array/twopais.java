f6409ffe0bc9013b69a334e8878eaea0869035f2
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-03-14 01:04:07 +0530

    updated

--- src/array/twopais.java
+++ src/array/twopais.java
@@ -2,13 +2,16 @@ package array;
 
 public class twopais {
     public static void printpairs(int numbers []){
+        int tp=0; // declare new vairable
         for(int i=0; i<numbers.length;i++){
             int current=numbers[i]; //2,3,4,5,6,7,
             for(int j=i+1;j<numbers.length;j++){
                 System.out.print("("+current+","+numbers[j]+")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total pairs  = " + tp); // it will print the total  pairses of the array
 
     }
     public static void main(String[] args) {
