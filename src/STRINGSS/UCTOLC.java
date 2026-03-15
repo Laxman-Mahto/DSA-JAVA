8147bfc6d5896e89b2de664b0e05486e5fcf11bb
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-03-16 02:20:51 +0530

    updated

---
+++ src/STRINGSS/UCTOLC.java
@@ -0,0 +1,18 @@
+package STRINGSS;
+
+public class UCTOLC {
+    //ek function bna lete hai jo LOWER CASE KO UPPER CASE KREGA
+    public static String toUpperCase(String str){
+        StringBuilder sb= new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=0;1<str.length();i++){
+
+        }
+    }
+    public static void main(String[] args) {
+        String str="hii, i am laxman";
+
+    }
+}
