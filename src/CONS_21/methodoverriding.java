a08f450f2a45ea20d0e40947c6394e8190abb05e
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-03-19 03:20:58 +0530

    updated

--- src/CONS_21/methodoverriding.java
+++ src/CONS_21/methodoverriding.java
@@ -1,4 +1,26 @@
 package CONS_21;
 
 public class methodoverriding {
+    public static void main(String[] args) {
+
+        animaal a = new animaal();
+        a.eats();
+
+    }
 }
+class animaal{
+        void eats() {
+            System.out.println("eating chicken");
+
+        }
+}
+
+class deer extends animaal{
+    void eats(){
+        System.out.println("maalik not given grass hehe");
+    }
+}
+
+
+
+//diff class but same funtion inside class na diff parameter/definatiom
